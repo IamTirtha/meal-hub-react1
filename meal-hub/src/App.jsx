f06cd9f4c50776cbb1349fd1c6d21e021c04d453
IamTirtha <tsd2580@gmail.com>
2026-04-24 22:22:53 +0530

    api fetched from mealhub

--- meal-hub/src/App.jsx
+++ meal-hub/src/App.jsx
@@ -1,12 +1,25 @@
 
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./Component/Navbar/navbar";
+import Foodsection from "./Component/Foodsection/Foodsection";
 
 function App() {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a") // fetch korlam
+      .then((res) => res.json()) // fetched data res e save korlam, jason format a convert korlam
+       .then((data) => setFoods(data.meals || [])); //data ke ui e update korlam through setFoods
+  }, []);
+
   return (
     <>
+      <Navbar></Navbar>
       
+        <Foodsection foodData={foods}></Foodsection>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ meal-hub/src/Component/Navbar/navbar.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+import mealImage from '../../assets/meal-photo.jpg'
+    const Navbar = () => {
+    return (
+        <>
+        <nav className="navbar">
+        <div className="leftnav">
+        <img src={mealImage} className="mealImg" alt="logo" />
+        <p>Meal-Hub</p>
+        </div>
+
+        <div>
+        <ul className="rightnav">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#order">Order</a></li>
+        </ul>
+        </div>
+    </nav>
+        </>
+    );
+    };
+
+    export default Navbar;
